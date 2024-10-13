67f1f93549e167dca8536e4681ffb8c46fe8ef4e
Vimmi05 <vimmituli1999@gmail.com>
2024-10-13 12:47:56 +0530

    index file

--- website/models.py
+++ website/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+# from django.db import models
+# from django.contrib.auth.models import User
 
-# Create your models here.
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     title = models.CharField(max_length=100)
+#     image = models.ImageField(upload_to='profile_pics', default='default.jpg')
+
+#     def __str__(self):
+#         return f'{self.user.username} Profile'
+
+# class Post(models.Model):
+#     author = models.ForeignKey(User, on_delete=models.CASCADE)
+#     content = models.TextField()
+#     image = models.ImageField(upload_to='post_images', blank=True, null=True)
+#     created_at = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return f'{self.author.username} - {self.created_at.strftime("%Y-%m-%d %H:%M")}'
--- website/views.py
+++ website/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render, redirect
-
+from website.models import *
 # Create your views here.
 def Index(request):
+    # posts = Post.objects.all().order_by('-created_at')
+    # context = {
+    #     'user': request.user,
+    #     'posts': posts,
+    # }
     return render(request, 'website/index.html')
+                #   , context)
